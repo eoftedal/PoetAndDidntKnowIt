e2a0f95e02374667befedc1f4625107e3206c2d5
Erlend Oftedal <erlend@oftedal.no>
2010-10-01 20:28:18 +0200

    First commit

---
+++ PoetAndDidntKnowIt/AesCrypt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace PoetAndDidntKnowIt
+{
+    public class AesCrypt : ICrypt
+    {
+        Aes aes;
+
+        public AesCrypt()
+        {
+            aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.GenerateIV();
+            aes.GenerateKey();
+            aes.Padding = PaddingMode.PKCS7;
+        }
+
+        public byte[] Encrypt(string clearText)
+        {
+            var bytes = Encoding.UTF8.GetBytes(clearText);
+            var cipher = aes.CreateEncryptor().TransformFinalBlock(bytes, 0, bytes.Length);
+            return aes.IV.Concat(cipher).ToArray();
+        }
+        public string Decrypt(byte[] iv, byte[] cipher)
+        {
+            var bytes = aes.CreateDecryptor(aes.Key, iv).TransformFinalBlock(cipher, 0, cipher.Length);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+    }
+}
---
+++ PoetAndDidntKnowIt/Wrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Web.Configuration;
+using System.Configuration;
+
+namespace PoetAndDidntKnowIt
+{
+    class MachineKeyWrapper
+    {
+        private static MethodInfo _encrypt;
+        private static MethodInfo _decrypt;
+
+        static MachineKeyWrapper()
+        {
+            MachineKeySection config = (MachineKeySection)ConfigurationManager.GetSection("system.web/machineKey");
+            Type machineKeyType = config.GetType().Assembly.GetType("System.Web.Configuration.MachineKeySection");
+            BindingFlags bf = BindingFlags.NonPublic | BindingFlags.Static;
+            _encrypt = typeof(System.Web.UI.Page).GetMethod("EncryptString", bf, null, new[] { typeof(string) }, null);
+            _decrypt = typeof(System.Web.UI.Page).GetMethod("DecryptString", bf, null, new[] { typeof(string) }, null);
+        }
+
+        public static string EncryptString(string s)
+        {
+            return (string)_encrypt.Invoke(null, new[] { s });
+        }
+        public static string DecryptString(string s)
+        {
+            return (string)_decrypt.Invoke(null, new[] { s });
+        }
+    }
+}
